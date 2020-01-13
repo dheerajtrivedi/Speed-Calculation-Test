c0853d24d74c3736a5765f6e9ae1b442f7c8b56a
Dheeraj <dheeraj.trivedi17@gmail.com>
2020-01-13 18:29:08 +0530

    Timer is now working

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let correctAnswers = 0;
 let wrongAnswers = 0;
 let notAttempted = 0;
-let totalQuestions = 2;
+let totalQuestions = 10;
 let questionNumber = 0;
 let firstNumberList = [];
 let secondNumberList = [];
@@ -12,11 +12,12 @@ let questionResult = [] //0:Not-answered -1:Incorrect 1:Correct
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = '+';
+let startTime;
 const indtroductionDiv = document.getElementById('introduction-box');
 const resultDiv = document.getElementById('result');
 const startButton = document.getElementById('start-button');
 const timerDiv = document.getElementById('timer');
-const timeSpan = document.getElementById('timer-content')
+const timerSpan = document.getElementById('timer-content')
 const restartButton = document.getElementById('restart-button');
 const quizBoxDiv = document.getElementById('quiz-box');
 const firstNumberSpan = document.getElementById('first-number');
@@ -44,11 +45,13 @@ function updateResultDiv() {
     document.getElementById('total-questions').innerHTML = totalQuestions;
     document.getElementById('total-correct').innerHTML = correctAnswers;
     document.getElementById('total-incorrect').innerHTML = wrongAnswers;
+    document.getElementById('time-taken').innerHTML = (Math.floor(timeSinceStart/100))/10 + "s";
 
 }
 function endQuiz(){
     hideElement(quizBoxDiv)
     showElement(resultDiv);
+    stopTimer();
     updateResultDiv();
 }
 function updateQuizDiv() {
@@ -65,8 +68,8 @@ function nextQuestion() {
         endQuiz();
         return;
     }
-    firstNumber = Math.floor(Math.random()*9 + 1);
-    secondNumber = Math.floor(Math.random()*9 + 1);
+    firstNumber = Math.floor(Math.random()*8 +2);
+    secondNumber = Math.floor(Math.random()*8 + 2);
     operator =  Math.floor(Math.random()*2);
 
     firstNumberList[questionNumber] = firstNumber;
@@ -85,9 +88,21 @@ function nextQuestion() {
     updateQuizDiv();
     console.log(`Question: ${firstNumber} ${operator} ${secondNumber} = ${questionAnswers[questionNumber]}`);
 }
+function updateTimerDiv() {
+    timeSinceStart = Date.now() - startTime;
+    timerSpan.innerHTML = Math.floor(timeSinceStart/100)/10;
+}
+function stopTimer() {
+    clearInterval(timerIntervalID);
+}
+function startTimer(){
+    startTime = Date.now();
+    timerIntervalID = setInterval(updateTimerDiv, 100) 
+}
 function startQuiz() {
     console.log("Quiz has started!");
     questionNumber = 0;
+    startTimer();
     nextQuestion();
     hideElement(indtroductionDiv);
     showElement(quizBoxDiv);
